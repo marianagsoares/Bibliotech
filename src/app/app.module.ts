7d2a3926048b591ab733695e97c8f3d023c898fe
marianagsoares <marianasoares.ti@gmail.com>
2022-11-25 14:20:01 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './telas/login/login.component';
+import { HomeComponent } from './telas/home/home.component';
+import { CadastrarComponent } from './telas/cadastrar/cadastrar.component';
+import { CadastroComponent } from './telas/cadastro/cadastro.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditarEmprestimoComponent } from './telas/editar-emprestimo/editar-emprestimo.component';
+import { NovoEmprestimoComponent } from './telas/novo-emprestimo/novo-emprestimo.component';
+import { PainelDeControleComponent } from './telas/painel-de-controle/painel-de-controle.component';
+import { CadastrarUsuarioComponent } from './telas/cadastrar-usuario/cadastrar-usuario.component';
+import { LoginUsuarioComponent } from './telas/login-usuario/login-usuario.component';
+import { CadastrarLivroComponent } from './telas/cadastrar-livro/cadastrar-livro.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { DetalhesComponent } from './componentes/detalhes/detalhes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    CadastrarComponent,
+    CadastroComponent,
+    EditarEmprestimoComponent,
+    NovoEmprestimoComponent,
+    PainelDeControleComponent,
+    CadastrarUsuarioComponent,
+    LoginUsuarioComponent,
+    CadastrarLivroComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    DetalhesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/telas/painel-de-controle/painel-de-controle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-de-controle',
+  templateUrl: './painel-de-controle.component.html',
+  styleUrls: ['./painel-de-controle.component.css']
+})
+export class PainelDeControleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
