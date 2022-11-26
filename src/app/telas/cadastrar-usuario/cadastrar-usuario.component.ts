3f8066ee5853c091e9b4982865612dd76e3ea712
polvinh0 <ieiel_4@hotmail.com>
2022-11-26 20:21:01 -0300

    login e cadastro de usuarios

--- src/app/telas/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/telas/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/modelos/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-cadastrar-usuario',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  public formCadastro: FormGroup;
+
+  constructor(fb: FormBuilder, private authService: AuthService, private router: Router, private notification: NotificationService) {
+    this.formCadastro = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  public signInGoogle(): void {
+    this.authService.authenticateByGoogle().subscribe(credencials => {
+      this.notification.showMessage("Autenticado com Google!");
+    }) 
+  }
+  
+
+  public createrUserEmailAndPassword(): void {
+    if(this.formCadastro.valid) {
+      const user: User = this.formCadastro.value;
+      this.authService.createUserEmailAndPassword(user).subscribe(response => {
+        this.notification.showMessage("Usuario Cadastrado.");
+        this.router.navigate(["/login"])
+      })
+    }
+    else {
+      this.notification.showMessage("Dados Inválidos")
+    }
+    
   }
 
 }
